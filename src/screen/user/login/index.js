49f0af39110aa3d8fe28c58bfc7925bad72c2a29
22-JWL <jck1004ee@gmail.com>
2023-10-24 23:48:34 +0900

    feat:이메일, 비밀번호 유효성 검사

--- src/screen/user/login/index.js
+++ src/screen/user/login/index.js
@@ -4,24 +4,69 @@ import './style.scss';
 import logo from './logo1beatbay_logo.jpg';
 import kakaologo from './kakako-icon.png';
 
-Modal.setAppElement('#root'); // 모달이 나타날 부모 요소 지정
+Modal.setAppElement('#root');
 
 const LoginModal = ({ isOpen, onRequestClose }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+  const [formErrors, setFormErrors] = useState({ email: '', password: '' });
+  const [loginError, setLoginError] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    setFormErrors({ ...formErrors, [name]: '' });
+    setLoginError('');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // 여기에서 서버로 데이터를 보내거나 다른 로그인 처리 로직을 구현합니다.
-    onRequestClose(); // 로그인 후 모달을 닫음
+
+    const errors = {};
+    if (!formData.email) {
+      errors.email = '이메일은 필수입니다.';
+    } else if (!isValidEmail(formData.email)) {
+      errors.email = '올바른 이메일 형식이 아닙니다.';
+    }
+
+    if (!formData.password) {
+      errors.password = '비밀번호는 필수입니다.';
+    } else if (formData.password.length < 8) {
+      errors.password = '비밀번호는 최소 8자 이상이어야 합니다.';
+    } else if (!isStrongPassword(formData.password)) {
+      errors.password =
+        '비밀번호는 최소 8자 이상, 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다.';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+    } else {
+      // 실제 로그인 로직을 구현하세요.
+      // 실패 시 setLoginError 함수를 사용하여 오류 메시지를 설정하세요.
+      if (loginFailed) {
+        setLoginError('로그인 실패: 이메일 또는 비밀번호가 올바르지 않습니다.');
+      } else {
+        onRequestClose();
+      }
+    }
   };
+
+  const isValidEmail = (email) => {
+    // 더 엄격한 이메일 형식 검사
+    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return emailPattern.test(email);
+  };
+
+  const isStrongPassword = (password) => {
+    // 비밀번호가 대문자, 소문자, 숫자, 특수 문자를 포함하는지 검사
+    const passwordPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordPattern.test(password);
+  };
+
+  const loginFailed = false; // 실제 로그인 실패 여부에 따라 설정
 
   return (
     <Modal
@@ -30,7 +75,7 @@ const LoginModal = ({ isOpen, onRequestClose }) => {
       className="login-modal"
       overlayClassName="modal-overlay"
     >
-      <img className="logo" src={logo}></img>
+      <img className="logo" src={logo} alt="로고"></img>
       <h2>BEAT BAY로 중고거래를 시작하세요</h2>
       <form onSubmit={handleSubmit} className="login-form">
         <div>
@@ -40,9 +85,13 @@ const LoginModal = ({ isOpen, onRequestClose }) => {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="form-input"
+            className={`form-input ${formErrors.email ? 'error' : ''}`}
             placeholder="아이디 / 이메일"
           />
+          <br></br>
+          {formErrors.email && (
+            <span className="error-message">{formErrors.email}</span>
+          )}
         </div>
         <div>
           <label className="form-label"></label>
@@ -51,16 +100,21 @@ const LoginModal = ({ isOpen, onRequestClose }) => {
             name="password"
             value={formData.password}
             onChange={handleChange}
-            className="form-input"
+            className={`form-input ${formErrors.password ? 'error' : ''}`}
             placeholder="비밀번호"
           />
+          <br></br>
+          {formErrors.password && (
+            <span className="error-message">{formErrors.password}</span>
+          )}
         </div>
+        {loginError && <p className="error-message">{loginError}</p>}
         <button type="submit" className="submit-button">
           로그인
         </button>
         <br></br>
         <button type="submit" className="kakao-submit-button">
-          <img src={kakaologo}></img>카카오 로그인
+          <img src={kakaologo} alt="카카오 로고"></img>카카오 로그인
         </button>
         <br></br>
         회원이 아니신가요? 회원가입하기
