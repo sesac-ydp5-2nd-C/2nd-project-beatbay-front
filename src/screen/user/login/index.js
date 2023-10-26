850ecbec5448fabadb066dfbf770901792952a75
22-JWL <jck1004ee@gmail.com>
2023-10-26 18:14:52 +0900

    feat:login animation

--- src/screen/user/login/index.js
+++ src/screen/user/login/index.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+// 스타일 파일 추가
+import './style.scss';
 
-export default function LoginScreen() {
-  return <div>LoginScreen</div>;
-}
+const LoginScreen = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUp(false);
+  };
+  return (
+    <div className={`container ${isSignUp ? 'right-panel-active' : ''}`}>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className={`form-container sign-in-container ${isSignUp && 'temp'}`}>
+        <form action="#">
+          <h1>Sign in</h1>
+          <span>or use your account</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div
+          className={`overlay ${isSignUp ? 'overlay-right' : 'overlay-left'}`}
+        >
+          <h1>{isSignUp ? 'Hello, Friend!' : 'Welcome Back!'}</h1>
+          <p>
+            {isSignUp
+              ? 'Enter your personal details and start your journey with us'
+              : 'To keep connected with us please login with your personal info'}
+          </p>
+          <button
+            className="ghost"
+            onClick={isSignUp ? handleSignInClick : handleSignUpClick}
+          >
+            {isSignUp ? 'Sign In' : 'Sign Up'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
