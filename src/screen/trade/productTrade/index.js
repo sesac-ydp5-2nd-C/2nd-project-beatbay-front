c0dcfdba0efecb423f2a49981bb81ce44ce0bc01
Sangwoo97 <aa6618@naver.com>
2023-10-26 19:52:39 +0900

    feat: 물품 거래 페이지 제작 중 (#10)

--- src/screen/trade/productTrade/index.js
+++ src/screen/trade/productTrade/index.js
@@ -2,15 +2,48 @@ import React, { useEffect, useRef, useState } from 'react';
 import './styles.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import Screen from '../../Screen';
+import CustomTab from '../../../components/customTab/CustomTab';
 
 function ProductTradeScreen() {
   const authInfo = useSelector((state) => state.user.authInfo);
+  const tabsData = [
+    {
+      id: 1,
+      title: '악기',
+      content: [
+        '관악기',
+        '현악기',
+        '타악기',
+        '건반악가',
+        '전자악기',
+        '앰프/스피커',
+        '악기용품',
+        '기타',
+      ],
+    },
+    {
+      id: 2,
+      title: '음반',
+      content: ['CD', 'DVD', 'LP', '기타'],
+    },
+  ];
 
   useEffect(() => {}, []);
 
   return (
     <Screen>
-      <div className="pTContainer"></div>
+      <CustomTab tabsData={tabsData} />
+
+      <div className="button-container">
+        <div className="button-list">
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+          <button>Button 4</button>
+          <button>Button 5</button>
+          <button>Button 6</button>
+        </div>
+      </div>
     </Screen>
   );
 }
