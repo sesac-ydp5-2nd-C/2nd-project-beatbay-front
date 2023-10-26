8e49206e2d989154a48abe4ed0ada1a06130ca4f
jjsh03 <aberrant03@gmail.com>
2023-10-26 16:40:55 +0900

    feat: 마이페이지 리뷰 컴포넌트

--- src/screen/mypage/dashboard.js
+++ src/screen/mypage/dashboard.js
@@ -1,12 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
 import Screen from '../Screen';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAuthInfo } from '../../store/feature/userSlice';
 import MypageMenus from '../../components/Mypage/MypageMenus';
 import Mypageprofile from '../../components/Mypage/Mypageprofile';
+import MypageReview from '../../components/Mypage/MypageReview';
 
 function MypageDashboard() {
+  const [reviewData, setReviewData] = useState([
+    {
+      userImg: 'profile_default.png',
+      userName: '강백호',
+      userType: 'Buyer',
+      reviewContent: '배고프다',
+    },
+    {
+      userImg: 'profile_default.png',
+      userName: '정대만',
+      userType: 'Buyer',
+      reviewContent: '잠온다',
+    },
+    {
+      userImg: 'profile_default.png',
+      userName: '이명헌',
+      userType: 'Seller',
+      reviewContent:
+        'Proident voluptate veniam voluptate mollit reprehenderit anim officia et ea ex laboris nulla laboris. Nulla ut aliquip fugiat tempor veniam sint aliqua reprehenderit tempor Lorem commodo anim.',
+    },
+  ]);
   const authInfo = useSelector((state) => state.user.authInfo);
 
   const dispatch = useDispatch();
@@ -41,6 +63,19 @@ function MypageDashboard() {
           </div>
 
           <hr />
+
+          <div className="reviews">
+            {reviewData.map((e, i) => {
+              return (
+                <MypageReview
+                  userImg={e.userImg}
+                  userName={e.userName}
+                  userType={e.userType}
+                  reviewContent={e.reviewContent}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </Screen>
