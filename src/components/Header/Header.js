d83b474b5ce66f220b02bf2a1bb094e96b0aa15b
22-JWL <jck1004ee@gmail.com>
2023-10-26 16:13:14 +0900

    fix:prettify

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ReactComponent as Logo } from '../../asset/beatbay_logo.svg';
 import './styles.scss';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom/dist';
 
 export default function Header({ color = 'black' }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // 메뉴를 열고 닫는 상태값 저장
@@ -22,10 +23,10 @@ export default function Header({ color = 'black' }) {
         <Logo className="logo" />
         <div style={{ display: 'flex' }}>
           <div style={{ color }} className="headerMenu mSpace">
-            SIGN UP
+            <Link to="/user/signup">SIGN UP</Link>
           </div>
           <div style={{ color }} className="headerMenu">
-            SIGN IN
+            <Link to="/user/login">SIGN IN</Link>
           </div>
         </div>
       </div>
--- src/router/App.js
+++ src/router/App.js
@@ -7,6 +7,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainScreen />} />
+          <Route path="/user/signup" element={<SignupScreen />} />
+          <Route path="/user/login" element={<LoginScreen />} />
         </Routes>
       </BrowserRouter>
     </div>
