3f6e9030491119aee726c28e328e71ef5d36ed45
이재욱 <ethan@ijaeug-ui-MacBookAir-2.local>
2023-10-27 14:30:16 +0900

    fix: classname

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    {/* <App /> */}
-    <MypageDashboard />
+    <App />
   </Provider>,
   // </React.StrictMode>
 );
