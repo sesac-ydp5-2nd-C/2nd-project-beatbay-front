b17e17994cc95df06589dc8391fbfee56a3258cd
jjsh03 <aberrant03@gmail.com>
2023-11-02 04:46:25 +0900

    feat: mypage Modal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './router/App';
 import { Provider } from 'react-redux';
 import store from './store';
 import './config/Fonts/Font.css';
-import MypageDashboard from './screen/mypage/dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/screen/mypage/dashboard/index.js
+++ src/screen/mypage/dashboard/index.js
@@ -6,7 +6,9 @@ import { setAuthInfo } from '../../../store/feature/userSlice';
 import MypageMenus from '../../../components/mypageMenu/MypageMenus';
 import MypageProfile from '../../../components/mypageProfile/MypageProfile';
 import MypageVinyl from '../../../components/mypageVinyl/MypageVinyl';
+
 import userImg from '../../../asset/profile_default.png';
+import CustomModal from '../../../components/common/customModal/CustomModal';
 
 function MypageDashboardScreen() {
   const [userData, setUserData] = useState({
@@ -24,11 +26,42 @@ function MypageDashboardScreen() {
 
   const authInfo = useSelector((state) => state.user.authInfo);
 
-  const dispatch = useDispatch();
+  const [reviewsData, setReviewsData] = useState(
+    {
+      user_nickname: '대만',
+      review: '좋아요',
+    },
+    { user_nickname: '호열', review: '별루에용' },
+    {
+      user_nickname: '백호',
+      review: '굿이에용',
+    },
+  );
 
-  useEffect(() => {
-    dispatch(setAuthInfo(1));
-  }, []);
+  const [followData, setFollowData] = useState(
+    {
+      user_nickname: '대만',
+      imgSrc: userImg,
+    },
+    { user_nickname: '호열', imgSrc: userImg },
+    {
+      user_nickname: '백호',
+      imgSrc: userImg,
+    },
+  );
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const openModal = (data) => {
+    setModalData(data);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+    setModalIsOpen(false);
+  };
 
   return (
     <Screen>
@@ -42,15 +75,24 @@ function MypageDashboardScreen() {
                 <h2>상품</h2>
                 <p>{userData.itemCount}</p>
               </div>
-              <div className="figure followers">
+              <div
+                className="figure followers"
+                onClick={() => openModal(reviewsData)}
+              >
                 <h2>후기</h2>
                 <p>{userData.user_review}</p>
               </div>
-              <div className="figure followers">
+              <div
+                className="figure followers"
+                onClick={() => openModal(followData)}
+              >
                 <h2>팔로워</h2>
                 <p>{userData.user_follower}</p>
               </div>
-              <div className="figure following">
+              <div
+                className="figure following"
+                onClick={() => openModal(followData)}
+              >
                 <h2>팔로잉</h2>
                 <p>{userData.user_following}</p>
               </div>
@@ -59,6 +101,13 @@ function MypageDashboardScreen() {
         </div>
       </div>
       <MypageMenus userData={userData} />
+      {modalIsOpen && (
+        <CustomModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          modalData={modalData}
+        />
+      )}
     </Screen>
   );
 }
