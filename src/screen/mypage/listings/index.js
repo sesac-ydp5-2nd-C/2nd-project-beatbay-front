b1db2c40789b25a17660f8804f8e436a8f64c1b3
jjsh03 <139740067+jjsh03@users.noreply.github.com>
2023-11-06 14:27:13 +0900

    feat: 마이페이지 탭 type 추가

--- src/screen/mypage/listings/index.js
+++ src/screen/mypage/listings/index.js
@@ -19,16 +19,20 @@ export default function MypageListingsScreen() {
     {
       id: 1,
       title: '상품',
+      type: 'product',
     },
     {
       id: 2,
       title: '재능',
+      type: 'ability',
     },
   ];
 
   const items = ['전체', '판매중', '판매완료'];
   const [showDropdown, setShowDropdown] = useState(false);
   const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [activeTab, setActiveTab] = useState(tabsData[0]);
+
   const [productData, setProductData] = useState(
     new Array(8).fill({
       title: '텔레캐스터 민트 팝니다',
@@ -53,7 +57,11 @@ export default function MypageListingsScreen() {
                 <MypageVinyl userData={userData} />
               </div>
             </div>
-            <MypageTab tabsData={tabsData} />
+            <MypageTab
+              tabsData={tabsData}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+            />
 
             <CustomDropdown
               showDropdown={showDropdown}
@@ -77,7 +85,13 @@ export default function MypageListingsScreen() {
             >
               <div className="MpGridContainer">
                 {productData.map((e, i) => {
-                  return <TradeCard key={`${i}_${e.title}`} data={e} />;
+                  return (
+                    <TradeCard
+                      key={`${i}_${e.title}`}
+                      data={e}
+                      type={activeTab.type}
+                    />
+                  );
                 })}
               </div>
             </InfiniteScroll>
