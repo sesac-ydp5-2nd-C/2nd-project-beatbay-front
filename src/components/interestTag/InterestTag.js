01cad0d85c6e173944d7044ff1b6994c86802ce7
22-JWL <jck1004ee@gmail.com>
2023-11-09 13:41:13 +0900

    feat: 마이페이지 탈퇴

--- src/components/interestTag/InterestTag.js
+++ src/components/interestTag/InterestTag.js
@@ -3,15 +3,18 @@ import Interest from './Interests';
 // import './style.scss';
 
 export default function InterestTag({ userData }) {
-  // const temp = {
-  //   user_interest: ['집', '가고', '싶다'],
-  // };
+  const temp = {
+    user_interest: ['관심사를 입력해주세요'],
+  };
   return (
     <div className="interestBox">
-      {userData?.user_interest &&
-        userData.user_interest.map((interest, index) => (
-          <Interest key={index} kind={interest} />
-        ))}
+      {userData?.user_interest
+        ? userData.user_interest.map((interest, index) => (
+            <Interest key={index} kind={interest} />
+          ))
+        : temp.user_interest.map((interest, index) => (
+            <Interest key={index} kind={interest} />
+          ))}
     </div>
   );
 }
