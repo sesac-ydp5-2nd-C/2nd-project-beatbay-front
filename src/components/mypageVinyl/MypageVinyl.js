635407057bbc3e23f6f117964860e3774ee0099a
jjsh03 <aberrant03@gmail.com>
2023-11-10 02:46:13 +0900

    fix: 마이페이지 데이터 에러 로딩 추가/#89

--- src/components/mypageVinyl/MypageVinyl.js
+++ src/components/mypageVinyl/MypageVinyl.js
@@ -24,7 +24,7 @@ export default function MypageVinyl({ userData }) {
         return mezzoforte;
       case 5:
         return fortissimo;
-
+      case 6:
       default:
         return pianissimo;
     }
