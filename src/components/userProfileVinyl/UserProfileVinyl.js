9dde9e192aaac11cdba6e83e4725f597237f1d86
22-JWL <jck1004ee@gmail.com>
2023-11-09 22:56:11 +0900

    base64->원본

--- src/components/userProfileVinyl/UserProfileVinyl.js
+++ src/components/userProfileVinyl/UserProfileVinyl.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './style.scss';
 import turntable from '../../asset/change_profile_img1.svg';
 import userImg from '../../asset/profile_default.png';
+import { useDropzone } from 'react-dropzone';
 
 export default function UserProfileVinyl({
   userData,
@@ -9,6 +10,20 @@ export default function UserProfileVinyl({
   setUploadedImage,
 }) {
   const [profileImage, setProfileImage] = useState('');
+
+  const onDrop = (acceptedFiles) => {
+    if (acceptedFiles.length > 1) {
+      alert('이미지는 최대 5개까지 업로드할 수 있습니다.');
+    } else {
+      setUploadedImage(acceptedFiles);
+      console.log(acceptedFiles);
+    }
+  };
+
+  const handleImageDelete = (index) => {
+    setUploadedImage();
+  };
+
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     console.log('Selected file:', file); // Add this line
@@ -25,26 +40,41 @@ export default function UserProfileVinyl({
       reader.readAsDataURL(file);
     }
   };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    maxFiles: 1,
+  });
   return (
     <div className="zzprofileVinyl">
       <label htmlFor="imageUpload">
         <img src={turntable} alt="turntable" className="zzturntable" />
       </label>
-      <input
+      {/* <input
         type="file"
         id="imageUpload"
         style={{ display: 'none' }}
-        onChange={handleImageUpload}
-      />
-      <img
-        src={
-          uploadedImage ||
-          `${process.env.REACT_APP_BACK_IP}/uploads/${userData?.user_profile_img}`
-        }
-        onError={(e) => (e.target.src = userImg)}
-        alt="profileImg"
-        className="zzprofileImg"
-      />
+        // onChange={handleImageUpload}
+      /> */}
+      <div {...getRootProps()} className="dropzone">
+        <input
+          type="file"
+          id="imageUpload"
+          style={{ display: 'none' }}
+          {...getInputProps()}
+        />
+        <img
+          src={
+            uploadedImage
+              ? URL.createObjectURL(uploadedImage[0])
+              : `${process.env.REACT_APP_BACK_IP}/uploads/${userData?.user_profile_img}`
+          }
+          onError={(e) => (e.target.src = userImg)}
+          alt="profileImg"
+          className="zzprofileImg"
+        />
+      </div>
     </div>
   );
 }
